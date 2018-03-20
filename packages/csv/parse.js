8e7b1a8ce2bd2adb472f7596a0b8357c169cdece
Dave Meehan <dave_meehan@replicated.co.uk>
2018-03-20 20:35:22 +0000

    Implement record & field parser

--- packages/csv/parse.js
+++ packages/csv/parse.js
@@ -2,20 +2,218 @@
 
 import { Transform } from 'stream'
 
-class CSVParser extends Transform {
-  constructor(options?: duplexStreamOptions) {
-    super({
-      ...options,
-      objectMode: true,
-    })
+const States = {
+  Initial: 'Initial',
+  Field: 'Field',
+  QuotedField: 'QuotedField',
+  AfterQuotedField: 'AfterQuotedField',
+  RecordDelimiter: 'RecordDelimiter',
+  Complete: 'Complete',
+}
+
+type State = $Keys<typeof States>
+
+type Options = {
+  trim?: boolean
+}
+
+class CSVRecordParser {
+
+  state: State
+  currentField: string
+  fields: Array<string>
+  trim: boolean
+
+  constructor(options?: Options = {}) {
+    this.state = States.Initial
+    this.trim = options.trim || false
   }
 
-  _transform(chunk: Buffer | string | any, encoding: string, done: () => void) {
-    // this.push() each result
-    done()  // when chunk completely consumed
+  push(char: number): boolean {
+
+    switch (this.state) {
+
+      case States.Initial: {
+        this.currentField = ''
+        this.fields = []
+        this.state = States.Field
+        return this.push(char)
+      }
+
+      case States.Field: {
+        switch (char) {
+          case 0x2c: {  // comma
+            this.flush()
+            break
+          }
+          case 0x0d: {  // CR
+            this.flush()
+            this.state = States.RecordDelimiter
+            break
+          }
+          case 0x22: {  // Quote
+            if (this.currentField.length < 1) {
+              this.state = States.QuotedField
+              break
+            }
+          }
+          // eslint-disable-next-line: no-fallthrough
+          default: {
+            this.currentField = this.currentField.concat(String.fromCharCode(char))
+            break
+          }
+        }
+      }
+      break
+
+      case States.QuotedField: {
+        switch (char) {
+          case 0x22: {
+            this.state = States.AfterQuotedField
+            break
+          }
+
+          default: {
+            this.currentField = this.currentField.concat(String.fromCharCode(char))
+          }
+        }
+        break
+      }
+
+      case States.AfterQuotedField: {
+        switch (char) {
+
+          case 0x22: {  // quote
+            this.currentField = this.currentField.concat(String.fromCharCode(char))
+            this.state = States.QuotedField
+            break
+          }
+
+          case 0x2c: {  // comma
+            this.flush()
+            this.state = States.Field
+            break
+          }
+
+          case 0x0d: {  // CR
+            this.flush()
+            this.state = States.RecordDelimiter
+            break
+          }
+
+          default: {
+            throw new Error(`Unexpected character (0x${char.toString(16)}) after quote`)
+          }
+        }
+        break
+      }
+
+      case States.RecordDelimiter: {
+        switch (char) {
+          case 0x0a: {
+            this.state = States.Complete
+            return true
+          }
+        }
+        break
+      }
+
+      case States.Complete: {
+        throw new Error('unexpected push after record complete')
+      }
+    }
+
+    return false
+  }
+
+  flush() {
+    switch (this.state) {
+      case States.Field:
+      case States.AfterQuotedField: {
+        this.fields.push(this.trim ? this.currentField.trim() : this.currentField)
+        this.currentField = ''
+      }
+    }
+  }
+
+  finalise(): boolean {
+    if (this.state !== States.Initial) {
+      this.flush()
+      this.state = States.Complete
+      return true
+    }
+    return false
+  }
+
+  get(): Array<string> {
+    switch (this.state) {
+
+      case States.Complete: {
+        const fields = this.fields
+        this.fields = []
+        this.state = States.Initial
+        return fields
+      }
+
+      default: {
+        throw new Error('get() called when incomplete')
+      }
+    }
   }
 }
 
-export default function parse(options?: duplexStreamOptions): CSVParser {
-  return new CSVParser(options)
+class CSVParser extends Transform {
+
+  buffer: Buffer
+  decoder: CSVRecordParser
+
+  constructor(options?: Options, streamOptions?: duplexStreamOptions) {
+    super({
+      ...streamOptions,
+      readableObjectMode: true,
+    })
+    this.buffer = Buffer.alloc(0, '', 'utf8')
+    this.decoder = new CSVRecordParser({
+      trim: false,
+      ...options,
+    })
+  }
+
+  push(chunk: Buffer | string | any): boolean {
+    return super.push(chunk)
+  }
+
+  _transform(chunk: Buffer | string, encoding: string, done: () => void) {
+
+    // $FlowFixMe
+    const buffer = Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk, encoding)
+    // $FlowFixMe
+    this.buffer = Buffer.concat([this.buffer, buffer])
+
+    try {
+      for (const ch of this.buffer.values()) {
+        if (this.decoder.push(ch)) {
+          this.push(this.decoder.get())
+        }
+      }
+      done()
+    } catch(e) {
+      // @$FlowFixMe
+      this.destroy(e)
+    }
+
+
+  }
+
+  _flush(done: Function): void {
+    if (this.decoder.finalise()) {
+      this.push(this.decoder.get())
+    }
+    done()
+  }
+
+}
+
+export default function parse(options?: Options, streamOptions?: duplexStreamOptions): CSVParser {
+  return new CSVParser(options, streamOptions)
 }
--- packages/csv/parse.test.js
+++ packages/csv/parse.test.js
@@ -9,7 +9,6 @@ describe('parse', () => {
   StreamTest.versions.forEach(version => {
 
     describe(`for ${version} streams`, () => {
-
       it('exists', () => {
         expect(parse).toBeDefined()
       })
@@ -26,12 +25,297 @@ describe('parse', () => {
         const parser = parse()
         StreamTest[version].fromChunks([''])
         .pipe(parser)
-        .pipe(StreamTest[version].toText((err, text) => {
+        .pipe(StreamTest[version].toObjects((err, objects) => {
           if (err) throw err
-          expect(text).toEqual('')
+          expect(objects).toEqual([])
         }))
       })
 
+      describe('2.1 each record on a separate line', () => {
+
+        it('handles a single record', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['123456789\r\n'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '123456789' ],
+            ])
+            done()
+          }))
+
+        })
+
+        it('handles two records', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['123456789\r\nABCDEFGHI\r\n'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '123456789' ],
+              [ 'ABCDEFGHI' ],
+            ])
+            done()
+          }))
+
+        })
+
+      })
+
+      describe('2.2 optional final record delimiter', () => {
+
+        it('handles a single record', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['123456789'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '123456789' ],
+            ])
+            done()
+          }))
+
+        })
+
+        it('handles two records', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['123456789\r\nABCDEFGHI'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '123456789' ],
+              [ 'ABCDEFGHI' ],
+            ])
+            done()
+          }))
+
+        })
+
+      })
+
+      describe('2.4 comma separated fields', () => {
+
+        it('handles no fields', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['\r\n'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '' ],
+            ])
+            done()
+          }))
+
+        })
+
+        it('handles two fields', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['123456789,ABCDEFGHI'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '123456789', 'ABCDEFGHI' ],
+            ])
+            done()
+          }))
+
+        })
+
+        it('handles three fields', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['123456789,ABCDEFGHI,QWERTYIOP'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '123456789', 'ABCDEFGHI', 'QWERTYIOP' ],
+            ])
+            done()
+          }))
+
+        })
+
+      })
+
+      describe('2.5 double quotes', () => {
+
+        it('handles an empty quoted field', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['""'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '' ],
+            ])
+            done()
+          }))
+
+        })
+
+        it('handles a quoted field', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['"123456789"\r\n'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '123456789' ],
+            ])
+            done()
+          }))
+
+        })
+
+        it('handles a quoted field, no record delimiter', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['"123456789"'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '123456789' ],
+            ])
+            done()
+          }))
+
+        })
+
+        it('handles a quoted field followed by an unquoted field', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['"123456789",ABCDEFGHI\r\n'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '123456789', 'ABCDEFGHI' ],
+            ])
+            done()
+          }))
+
+        })
+
+      })
+
+      describe('2.6 fields with quotes, commas and newlines should be quoted', () => {
+
+        it('handles a comma within a quoted field', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['"1234,5678"'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '1234,5678' ],
+            ])
+            done()
+          }))
+
+        })
+
+        it('handles CRLF within a quoted field', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['"1234\r\n5678"'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '1234\r\n5678' ],
+            ])
+            done()
+          }))
+
+        })
+
+        it('errors on unexpected chars after quoted field', async () => {
+
+          function f() {
+            return new Promise((resolve, reject) => {
+              const parser = parse()
+              .on('error', reject)
+              .on('finish', resolve)
+              StreamTest[version].fromChunks(['"1234"5678'])
+              .pipe(parser)
+              .pipe(StreamTest[version].toObjects(() => undefined))
+            })
+          }
+
+          await expect(f()).rejects.toThrow(/Unexpected character \(0x35\)/i)
+        })
+
+      })
+
+      describe('2.7 quotes within quoted fields', () => {
+
+        it('handles an escaped quote within a quoted field', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks(['"1234""5678"'])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '1234"5678' ],
+            ])
+            done()
+          }))
+
+        })
+
+      })
+
+      describe('options', () => {
+
+        it('doesn\t trim by default', (done) => {
+
+          const parser = parse()
+          StreamTest[version].fromChunks([' 1234 5678 '])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ ' 1234 5678 ' ],
+            ])
+            done()
+          }))
+
+        })
+
+        it('trims', (done) => {
+
+          const parser = parse({ trim: true })
+          StreamTest[version].fromChunks([' 1234 5678 '])
+          .pipe(parser)
+          .pipe(StreamTest[version].toObjects((err, objects) => {
+            if (err) throw err
+            expect(objects).toEqual([
+              [ '1234 5678' ],
+            ])
+            done()
+          }))
+
+        })
+
+      })
     })
 
   })
